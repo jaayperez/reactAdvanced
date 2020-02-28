6abeb5b5248ab8f542fc51a774d606139c3420bc
Justin Perez <justin@startrev.com>
2020-02-27 21:07:16 -0800

    Add React Helmet to enhance SEO

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './app.css';
 import Header from './header';
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <div className="app">
+    <Helmet title= "React Advanced"
+ meta={[
+  {"name": "description", "content": "Description of page"},
+  {property: "og:title", content: "Example title"},
+  {property: "og:image", content: "/"},
+  {property: "og:url", content: "/"}
+ ]}
+/>
       <Header />
     </div>
   );
